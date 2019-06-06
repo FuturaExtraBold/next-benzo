dc114af358940a628c3d31ef72eb9299804b55b2
Ben Hays <ben@kajabi.com>
2019-06-06 12:08:20 -0700

    Got server and client matching for project data.

--- components/ProjectSlide.js
+++ components/ProjectSlide.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ProjectSlide = (props) => {
   return (
-    <Link href={ `projects/${ props.pretty }` }>
+    <Link href={ `/projects/${ props.pretty }` }>
       <div className="project">
         <img style={{ width: "60px", float: "left" }} src={ `http://www.davidahays.com/cdn/img/thumbs/${ props.thumb }` }/>
       </div>
