99c28184c3e82a19971cdcced0c9d438f5fd9742
Ben Hays <ben@kajabi.com>
2019-06-06 11:23:04 -0700

    Moved everything into the index.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
+import React, { Component } from "react";
 import Link from "next/link";
-import Header from "../components/header";
+import fetch from "isomorphic-unfetch";
 
-function Index() {
-  return (
-    <main>
-      <Header />
-      <section>
-        <Link href="/about">
-          <a>Go to About Me, Benzo!</a>
-        </Link>
-      </section>
-    </main>
-  );
+class Index extends Component {
+
+  static async getInitialProps() {
+    const res = await fetch("http://www.davidahays.com/cdn/json/new_data.json");
+    const json = await res.json();
+    return { data: {
+      clients: json.clients,
+      projects: json.projects
+    }};
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="clients">
+          {this.props.data.clients.map((client, index) => (
+            <img key={ index } style={{ width: "60px" }} src={ `http://www.davidahays.com/cdn/img/clients/${ client.img }` } alt={ client.alt } />
+          ))}
+        </div>
+        <div className="projects">
+          {this.props.data.projects.map((project, index) => (
+            <Link key={ project.pretty } href={ `projects/${ project.pretty }` }>
+              <div className="project">
+                <img style={{ width: "60px", float: "left" }} src={ `http://www.davidahays.com/cdn/img/thumbs/${ project.thumb }` }/>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Index;
